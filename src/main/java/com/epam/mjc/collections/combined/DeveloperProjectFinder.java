827729bd79aeac4c72022bb7772fed3a2899498e
romikokvinikadze <r.kvinikadze11@gmail.com>
2022-08-20 08:46:15 +0400

    final collections

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,25 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> res = new ArrayList<>();
+        for (String key: projects.keySet()){
+            if (projects.get(key).contains(developer)){
+                res.add(key);
+            }
+        }
+
+        res.sort(new SortProjectList());
+        return res;
+    }
+
+    public class SortProjectList implements Comparator<String> {
+
+        @Override
+        public int compare(String o1, String o2) {
+            return o1.length() == o2.length() ? o2.compareTo(o1) : o1.compareTo(o2);
+        }
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
+++ src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
@@ -1,10 +1,14 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class LessonsGetter {
     public Set<String> getLessons(Map<String, List<String>> timetable) {
+        Set<String> res = new HashSet<>();
+        for (List<String> ls : timetable.values()){
+            res.addAll(ls);
+        }
+
+        return res;
     }
 }
